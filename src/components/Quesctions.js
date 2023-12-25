cc017eee428e6e6b9200bcbf099299926c7a25f9
shravani <shravanekadam@gmail.com>
2023-12-25 12:39:48 +0530

    error solved

--- src/components/Quesctions.js
+++ src/components/Quesctions.js
@@ -1,44 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import data from '../database/data'
-
+import React, { useEffect, useState } from "react";
+import data from "../database/data";
 
 /**custom hook */
-import { useFetchQuiesction } from '../hook/FetchQuestion'
+import { useFetchQuesction } from "../hook/FetchQuestion";
 
 export default function Quesctions() {
-    const [checked, setChecked] = useState(undefined)
-    const [{isLoading,apiData,serverError}] = useFetchQuiesction()
-    const question = data[0]
+  const [checked, setChecked] = useState(undefined);
+  const [{ isLoading, apiData, serverError }] = useFetchQuesction();
+  const question = data[0];
 
-    useEffect(() => {
-      // console.log(isLoading);
-      // console.log(apiData);
-      // console.log(serverError);
+  useEffect(() => {
+    // console.log(isLoading);
+    // console.log(apiData);
+    // console.log(serverError);
+  });
 
-    })
-    
-    function onSelect(){
-        // console.log("radio button change ")
-    }
+  function onSelect() {
+    // console.log("radio button change ")
+  }
   return (
-    <div className='quesctions'>
-      <h2 className='text-light'>{question.question}</h2>
+    <div className="quesctions">
+      <h2 className="text-light">{question.question}</h2>
       <ul key={question.id}>
-        {
-          question.options.map((q, i) => (
-            <li key={i}>
-            <input type='radio'
-            value={false}
-            name="options"
-            id={`q${i}-option`}
-            onChange={onSelect()}
+        {question.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              value={false}
+              name="options"
+              id={`q${i}-option`}
+              onChange={onSelect()}
             />
-            <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
-            <div className='check'></div>
-        </li>
-          ))
-        }
+            <label className="text-primary" htmlFor={`q${i}-option`}>
+              {q}
+            </label>
+            <div className="check"></div>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
--- src/hook/FetchQuestion.js
+++ src/hook/FetchQuestion.js
@@ -1,43 +1,39 @@
-import { useEffect , useState } from "react"
-import {useDispatch} from 'react-redux';
-import data from '../database/data';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import data from "../database/data";
 
 /**redux action  */
-import * as Action from '../redux/question_reducer'
+import * as Action from "../redux/question_reducer";
 
 /**this is the fetch quesction hook to fetch api data and set value to store */
-export const useFetchQuiesction = () =>{
+export const useFetchQuesction = () => {
+  const dispatch = useDispatch();
+  const [getData, setGetData] = useState({ isLoading : false, apiData : [], serverError: null});
 
-    const dispatch = useDispatch();
-    const [getData,setGetData]=useState({
-        isLoading:false ,
-        apiData:[],
-        serverError : null
-    })
-    useEffect(()=>{
-            setGetData(prev=>({...prev, isLoading:true}))
-            /**async function fetch backend data  */
-            (async ()=>{
-                try{
-                    let question = await data ;
+    useEffect(() => {
+        setGetData(prev => ({...prev, isLoading : true}));
 
-                    if(question.length>0){
-                        setGetData(prev=>({...prev, isLoading:false}))
-                        setGetData(prev=>({...prev, apiData:question}))
+        /** async function fetch backend data */
+        (async () => {
+            try {
+          let question = await data;
 
-                        /**dispatch an action */
-                        dispatch(Action.startExamAction())
-                    }
-                    else {
-                        throw new Error("No quesction available")
-                    }
-                }catch(error){
-                    setGetData(prev=>({...prev, isLoading:false}))
-                    setGetData(prev=>({...prev, serverError:error}))
-                }
+          if (question.length > 0) {
+            setGetData((prev) => ({ ...prev, isLoading: false }));
+            setGetData((prev) => ({ ...prev, apiData: question }));
 
-            })();
-    },[dispatch]);
+            /**dispatch an action */
+            dispatch(Action.startExamAction());
+          } else {
+            throw new Error("No quesction available");
+          }
+        } catch (error) {
+          setGetData((prev) => ({ ...prev, isLoading: false }));
+          setGetData((prev) => ({ ...prev, serverError: error }));
+        }
+      }
+    )();
+  }, [dispatch]);
 
-    return [getData,setGetData];
-}
+  return [getData, setGetData];
+};
