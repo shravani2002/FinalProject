03c2c21ebde5402c2c4d6a5e94fe5cfc41d5010a
shravani <shravanekadam@gmail.com>
2023-12-27 20:35:08 +0530

    changes regarding prev and next

--- src/components/Quesctions.js
+++ src/components/Quesctions.js
@@ -1,48 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { useSelector } from "react-redux";
 /**custom hook */
 import { useFetchQuesction } from "../hook/FetchQuestion";
+import { updateResult } from "../hook/setResult";
 
-export default function Quesctions() {
-  const [checked, setChecked] = useState(undefined);
-  const [{ isLoading, apiData, serverError }] = useFetchQuesction();
- 
 
-  const questions = useSelector(state => state.questions.queue[state.questions.trace])
-  const trace = useSelector(state =>state.questions.trace)
+export default function Quesctions({onChecked}) {
 
-    useEffect(()=>{
-        // console.log(questions)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-    })
-  function onSelect() {
-    // console.log("radio button change ")
+    const [checked, setChecked] = useState(undefined)
+    const { trace } = useSelector(state => state.questions);
+    const result = useSelector(state => state.result.result);
+    const [{ isLoading, apiData, serverError}] = useFetchQuesction() 
+
+    const questions = useSelector(state => state.questions.queue[state.questions.trace])
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+      dispatch(updateResult({ trace, checked}))
+  }, [checked])
+  
+  function onSelect(i){
+      onChecked(i)
+      setChecked(i)
+      dispatch(updateResult({ trace, checked}))
   }
 
-  if(isLoading) return <h3 className="text-light">isLoading</h3>
-  if(serverError) return <h3 className="text-light">{serverError || "Unknown error"}</h3>
-
+  if(isLoading) return <h3 className='text-light'>isLoading</h3>
+    if(serverError) return <h3 className='text-light'>{serverError || "Unknown Error"}</h3>
 
   return (
-    <div className="quesctions">
-      <h2 className="text-light">{questions?.question}</h2>
-      <ul key={questions?.id}>
-        {questions?.options.map((q, i) => (
-          <li key={i}>
-            <input
-              type="radio"
-              value={false}
-              name="options"
-              id={`q${i}-option`}
-              onChange={onSelect()}
-            />
-            <label className="text-primary" htmlFor={`q${i}-option`}>
-              {q}
-            </label>
-            <div className="check"></div>
-          </li>
-        ))}
-      </ul>
+    <div className='questions'>
+        <h2 className='text-light'>{questions?.question}</h2>
+
+        <ul key={questions?.id}>
+            {
+                questions?.options.map((q, i) => (
+                    <li key={i}>
+                        <input 
+                            type="radio"
+                            value={false}
+                            name="options"
+                            id={`q${i}-option`}
+                            onChange={() => onSelect(i)}
+                        />
+
+                        <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+                        <div className={`check ${result[trace] == i ? 'checked' : ''}`}></div>
+                    </li>
+                ))
+            }
+        </ul>
     </div>
-  );
+  )
 }
